80773ca0d871503b7af412ed17cd0113ae72b90e
Akshay Raj Gollahalli <akshay.gollahalli@fisherpaykel.com>
2023-11-17 15:36:11 +1300

    feat: Add support for displaying file sizes in a table
    This commit adds support for displaying file sizes in a table format using the `comfy-table` crate. The code now creates a `Table` object and loads the UTF8_FULL preset to set the width of the table. It then iterates over the `sizes` hashmap and adds each entry (excluding the root directory) as a row to the table. Finally, it prints out the table.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::collections::HashMap;
 use std::io;
 use std::path::Path;
+use std::process::exit;
 
 use clap::Parser;
-use humansize::{DECIMAL, format_size};
+use comfy_table::presets::UTF8_FULL;
+use comfy_table::Table;
+use humansize::{format_size, DECIMAL};
 use walkdir::WalkDir;
 
 mod utils;
@@ -19,44 +22,48 @@ struct Args {
 fn main() {
     let cli = Args::parse();
     let mut sizes: HashMap<String, u64> = HashMap::new();
+    let mut table = Table::new();
 
-    for path in cli.path {
-        for entry in WalkDir::new(path) {
+    for input_path in cli.path {
+        for entry in WalkDir::new(&input_path) {
             match entry {
-                Ok(entry) => {
-                    match entry.metadata() {
-                        Ok(metadata) => {
-                            let root_name = utils::get_root(&entry.path().display().to_string()).display().to_string();
-                            if metadata.is_dir() {
-                                sizes.entry(root_name).or_insert(0);
-                            } else if metadata.is_file() {
-                                sizes.entry(root_name).and_modify(|root_name| *root_name += metadata.len()).or_insert(0);
-                            } else {
-                                println!("{} is unknown", entry.path().display());
-                            }
+                Ok(entry) => match entry.metadata() {
+                    Ok(metadata) => {
+                        let get_first = input_path == ".";
+                        let entry_path = entry.path().display().to_string();
+                        let root_name = utils::get_root(&entry_path, Some(get_first))
+                            .display()
+                            .to_string();
+                        if metadata.is_dir() {
+                            sizes.entry(root_name).or_insert(0);
+                        } else if metadata.is_file() {
+                            sizes
+                                .entry(root_name)
+                                .and_modify(|root_name| *root_name += metadata.len())
+                                .or_insert(0);
                         }
-                        Err(err) => println!("failed to get metadata: {}", err)
                     }
-                }
+                    Err(err) => {
+                        println!("failed to get metadata: {}", err);
+                        exit(1)
+                    }
+                },
                 Err(err) => {
                     let path = err.path().unwrap_or(Path::new("")).display();
                     println!("failed to access entry {}", path);
                     if let Some(inner) = err.io_error() {
                         match inner.kind() {
                             io::ErrorKind::InvalidData => {
-                                println!(
-                                    "entry contains invalid data: {}",
-                                    inner)
+                                println!("entry contains invalid data: {}", inner);
+                                exit(1)
                             }
                             io::ErrorKind::PermissionDenied => {
-                                println!(
-                                    "Missing permission to read entry: {}",
-                                    inner)
+                                println!("Missing permission to read entry: {}", inner);
+                                exit(1)
                             }
                             _ => {
-                                println!(
-                                    "Unexpected error occurred: {}",
-                                    inner)
+                                println!("Unexpected error occurred: {}", inner);
+                                exit(1)
                             }
                         }
                     }
@@ -65,9 +72,14 @@ fn main() {
         }
     }
 
+    table.load_preset(UTF8_FULL).set_width(80);
+    // table.set_header(vec!["Header1", "Header2", "Header3"]);
     // Print the sizes values
     for (root, size) in &sizes {
-        let sz = format_size(*size, DECIMAL);
-        println!("{root} has {sz} hp");
+        if root != "." {
+            let sz = format_size(*size, DECIMAL);
+            table.add_row(vec![root, &String::from(sz)]);
+        }
     }
+    println!("{table}")
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,31 +1,40 @@
 use std::path::{Path, PathBuf};
 
 /// Get the root of a path
-/// 
-/// # Arguments 
-/// 
+///
+/// # Arguments
+///
 /// * `path`: String - The path to get the root of
-/// 
-/// returns: PathBuf 
-/// 
-/// # Examples 
-/// 
+///
+/// returns: PathBuf
+///
+/// # Examples
+///
 /// ```
 /// let root = get_root(String::from("some/path"));
 /// ```
-pub(crate) fn get_root(path: &str) -> PathBuf {
-    let mut components = Path::new(path).components();
+pub fn get_root(path: &str, get_first: Option<bool>) -> PathBuf {
+    let first = get_first.unwrap_or(true);
 
-    // Initialize an empty PathBuf
-    let mut root = PathBuf::new();
+    let components: Vec<_> = Path::new(path).components().collect();
 
-    // Take the first two components and add them to root
-    if let Some(first_component) = components.next() {
-        root.push(first_component.as_os_str());
-        if let Some(second_component) = components.next() {
-            root.push(second_component.as_os_str());
+    if first {
+        // Take the first two components (if available) and add them to root
+        let mut root = PathBuf::new();
+        if let Some(first_component) = components.get(0) {
+            root.push(first_component.as_os_str());
+            if let Some(second_component) = components.get(1) {
+                root.push(second_component.as_os_str());
+            }
+        }
+        root
+    } else {
+        // Take the last component (if available) and add it to root
+        if let Some(last_component) = components.last() {
+            PathBuf::from(last_component.as_os_str())
+        } else {
+            PathBuf::new()
         }
     }
-
-    root
 }
+
